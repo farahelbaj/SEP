92f9a3af45202cdda32211835d36604219ff5b40
farahelb <farah.elb26@gmail.com>
2026-02-02 16:33:16 +0200

    Initial commit - TemperatureConverter with tests

---
+++ src/test/java/org/example/TemperatureConverterTest.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TemperatureConverterTest {
+    private TemperatureConverter converter;
+    @BeforeEach
+    void setUp() {
+        converter = new TemperatureConverter();
+    }
+    @Test
+    void fahrenheitToCelsiusTest(){
+        assertEquals(0.0, converter.fahrenheitToCelsius(32), 0.001);
+        assertEquals(100.0, converter.fahrenheitToCelsius(212), 0.001);
+        assertEquals(-40.0, converter.fahrenheitToCelsius(-40), 0.001);
+    }
+    @Test
+    void celsiusToFahrenheitTest(){
+        assertEquals(32.0, converter.celsiusToFahrenheit(0), 0.001);
+        assertEquals(212.0, converter.celsiusToFahrenheit(100), 0.001);
+        assertEquals(-40.0, converter.celsiusToFahrenheit(-40), 0.001);
+
+    }
+    @Test
+    void isExtremeTemperatureTest(){
+        assertTrue(converter.isExtremeTemperature(-55));
+        assertTrue(converter.isExtremeTemperature(70));
+        assertFalse(converter.isExtremeTemperature(0));
+        assertFalse(converter.isExtremeTemperature(50));
+    }
+}
